2f7b7c362639ebc5293e7ea47bc32ed335c845f8
Marcos <onetestt6@gmail.com>
2024-12-04 11:50:40 -0300

    tela criar_promocao

--- promocoes/urls.py
+++ promocoes/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	path('', views.home, name = 'home'),
 	path('criar_produto/', views.criar_produto, name = 'criar_produto'),
 	path('criar_empresa/', views.criar_empresa, name = 'criar_empresa'),
+    path('criar_promocao/', views.criar_promocao, name = 'criar_promocao')
 ]
